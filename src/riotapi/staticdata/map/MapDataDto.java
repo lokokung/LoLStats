2f655924e791b339a9ce4ff9e6e641e9f454859a
Loko Kung <lokokung@gmail.com>
2015-07-27 23:53:57 -0700

    Added riotapi.staticdata.map

---
+++ src/riotapi/staticdata/map/MapDataDto.java
@@ -0,0 +1,28 @@
+package riotapi.staticdata.map;
+
+import java.util.HashMap;
+
+public class MapDataDto {
+    private HashMap<String, MapDetailsDto> data;
+    private String type;
+    private String version;
+
+    public HashMap<String, MapDetailsDto> get_data(){
+        return data;
+    }
+    public void set_data(HashMap<String, MapDetailsDto> data){
+        this.data = data;
+    }
+    public String get_type(){
+        return type;
+    }
+    public void set_type(String type){
+        this.type = type;
+    }
+    public String get_version(){
+        return version;
+    }
+    public void set_version(String version){
+        this.version = version;
+    }
+}
---
+++ src/riotapi/staticdata/map/MapDetailsDto.java
@@ -0,0 +1,37 @@
+package riotapi.staticdata.map;
+
+import java.util.ArrayList;
+
+import riotapi.staticdata.image.ImageDto;
+
+public class MapDetailsDto {
+    private ImageDto image;
+    private long mapId;
+    private String mapName;
+    private ArrayList<Long> unpurchasableItemList;
+
+    public ImageDto get_image(){
+        return image;
+    }
+    public void set_image(ImageDto image){
+        this.image = image;
+    }
+    public long get_mapId(){
+        return mapId;
+    }
+    public void set_mapId(long mapId){
+        this.mapId = mapId;
+    }
+    public String get_mapName(){
+        return mapName;
+    }
+    public void set_mapName(String mapName){
+        this.mapName = mapName;
+    }
+    public ArrayList<Long> get_unpurchasableItemList(){
+        return unpurchasableItemList;
+    }
+    public void set_unpurchasableItemList(ArrayList<Long> unpurchasableItemList){
+        this.unpurchasableItemList = unpurchasableItemList;
+    }
+}
