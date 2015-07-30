1388ed619df80e854fe4def02be7c2c16cb16d69
Loko Kung <lokokung@gmail.com>
2015-07-30 00:59:51 -0700

    Renamed exception due to mispelling... Also added apiKey to be ignored.

---
+++ src/riotapi/exceptions/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package riotapi.exceptions;
+
+public class UnauthorizedException extends Exception {
+
+    public UnauthorizedException() {
+        super();
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
