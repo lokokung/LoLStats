9657aa2c2adfcbbc3622878910bef518cea53ccc
Loko Kung <lokokung@gmail.com>
2015-07-30 22:36:40 -0700

    Added match module for the riot api.

---
+++ src/riotapi/match/RiotAPIMatchModule.java
@@ -0,0 +1,59 @@
+package riotapi.match;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import riotapi.core.IRiotAPIModule;
+import util.URLHandler;
+
+public class RiotAPIMatchModule implements IRiotAPIModule{
+    private final String riotAPIMatch_version = "2.2";
+    private final String riotAPIMatch_match =
+            "https://%2$s.api.pvp.net/api/lol/%2$s/v%1$s/match/"
+                    + "%3$s?includeTimeline=true&api_key=%4$s";
+    private final String riotAPIMatch_matchhistory =
+            "https://%2$s.api.pvp.net/api/lol/%2$s/v%1$s/matchhistory/"
+                    + "%3$s?api_key=%4$s";
+    
+    private final Gson gson;
+    private final URLHandler urlHandler;
+    private final HashMap<Type, String> type_map;
+    
+    public RiotAPIMatchModule(Gson gson, URLHandler urlHandler){
+        this.gson = gson;
+        this.urlHandler = urlHandler;
+        
+        this.type_map = new HashMap<Type, String>();
+        Type matchDetail = new TypeToken<MatchDetail>() {
+        }.getType();
+        Type playerHistory = new TypeToken<PlayerHistory>() {
+        }.getType();
+        this.type_map.put(matchDetail, this.riotAPIMatch_match);
+        this.type_map.put(playerHistory, this.riotAPIMatch_matchhistory);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T queryAPI(ArrayList<String> args, Type objType)
+            throws Exception {
+        if (type_map.containsKey(objType)) {
+            String query =
+                    String.format(this.type_map.get(objType),
+                            riotAPIMatch_version, args.get(0), args.get(1),
+                            args.get(2));
+            String jsonString = urlHandler.requestGetString(query);
+            if (jsonString == null) {
+                return null;
+            }
+            
+            T result = (T) gson.fromJson(jsonString, objType);
+            return result;
+        }
+        return null;
+    }
+
+}
